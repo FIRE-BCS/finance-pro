356fc65a265c8abfd212903ea34652786a0b057e
Xin Ying <105095442+channxy@users.noreply.github.com>
2024-07-20 16:32:32 +0800

    new frontend first push

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,6 +1,7 @@
-import { supabaseKey, supabaseUrl } from '@/supabasekeys'
+
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+import { supabaseKey, supabaseUrl } from '../../supabasekeys'
 
 export function createClient() {
   const cookieStore = cookies()
