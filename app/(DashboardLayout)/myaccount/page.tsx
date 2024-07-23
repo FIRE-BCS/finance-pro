93cb50d9438cd164c7e1061610201abb09d0558c
Delven Wong Peng Yu <72071732+wongpengyu@users.noreply.github.com>
2024-07-23 17:10:47 +0800

    move folders + implement Router

--- app/(DashboardLayout)/myaccount/page.tsx
+++ app/(DashboardLayout)/myaccount/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Box, Grid, Paper, Typography } from "@mui/material";
-import BaseCard from '../../components/shared/BaseCard';
+import BaseCard from '../components/shared/BaseCard';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 
 const Item = styled(Paper)(({ theme }) => ({
